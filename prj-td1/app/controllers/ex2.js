864b39edfe82e11a834072078f06819c5a0f5fd6
Guillaume Lafosse <21701400@campus.unicaen.fr>
2019-01-31 17:32:16 +0100

    style + controller

--- prj-td1/app/controllers/ex2.js
+++ prj-td1/app/controllers/ex2.js
@@ -1,9 +1,17 @@
 import Controller from '@ember/controller';
+import { set } from '@ember/object';
 
 export default Controller.extend({
   actions: {
     toggleService(service) {
-      console.log(service[0].get('active'));
+
+      if (service.active) {
+        set(service, "active", false);
+      }
+
+      else if (service.active == false) {
+        set(service, "active", true);
+      }
     }
   }
 });
