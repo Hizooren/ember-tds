3b1a328520fadb19f9ec85a2b973c026638db56e
Hizoren <guillaume.lafosse14@gmail.com>
2019-02-03 13:35:21 +0100

    format currency + plural

---
+++ prj-td1/app/helpers/format-currency.js
@@ -0,0 +1,15 @@
+import { helper } from '@ember/component/helper';
+
+export function formatCurrency(params/*, hash*/) {
+  return params;
+}
+
+export default Ember.Helper.helper(function(params, namedArgs) {
+  let value = params[0],
+    euros = Math.floor(value),
+    cents = value % 10,
+    sign = namedArgs.sign === undefined ? '$' : namedArgs.sign;
+
+  if (cents.toString().length === 1) { cents = '0' + cents; }
+  return `${euros},${cents}${sign}`;
+});
---
+++ prj-td1/app/helpers/plural.js
@@ -0,0 +1,23 @@
+import { helper } from '@ember/component/helper';
+
+export function plural(params/*, hash*/) {
+  return params;
+}
+
+export default Ember.Helper.helper(function(params, namedArgs) {
+
+  let nbServices = params[0];
+  if (nbServices == 0){
+    return  `Aucun service n'est séléctionné.`;
+  }else if (nbServices == 4){
+    return  `Tous les services sont séléctionnés.`;
+
+  }else if(nbServices == 1){
+    return  `Vous avez ${params[0]} service séléctionné.`;
+
+  }else{
+      return  `Vous avez ${params[0]} services séléctionnés.`;
+  }
+});
+
+
